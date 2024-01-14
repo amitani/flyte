c3dc047a878943313393ad8cc0066c3f2a613926
Fabio Graetz <fabiograetz@googlemail.com>
2024-01-14 18:37:37 +0000

    Use core.PhaseInfoQueuedWithTaskInfo instead of core.PhaseInfoQueued in plugins so log links are available
    Signed-off-by: Fabio Graetz <fabiograetz@googlemail.com>

--- flyteplugins/go/tasks/plugins/k8s/ray/ray.go
+++ flyteplugins/go/tasks/plugins/k8s/ray/ray.go
@@ -46,7 +46,8 @@ var logTemplateRegexes = struct {
 	tasklog.MustCreateRegex("rayJobID"),
 }
 
-type rayJobResourceHandler struct{}
+type rayJobResourceHandler struct {
+}
 
 func (rayJobResourceHandler) GetProperties() k8s.PluginProperties {
 	return k8s.PluginProperties{}
@@ -127,8 +128,7 @@ func (rayJobResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsC
 			EnableIngress:  &enableIngress,
 			RayStartParams: headNodeRayStartParams,
 		},
-		WorkerGroupSpecs:        []rayv1alpha1.WorkerGroupSpec{},
-		EnableInTreeAutoscaling: &rayJob.RayCluster.EnableAutoscaling,
+		WorkerGroupSpecs: []rayv1alpha1.WorkerGroupSpec{},
 	}
 
 	for _, spec := range rayJob.RayCluster.WorkerGroupSpec {
@@ -139,6 +139,16 @@ func (rayJobResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsC
 			objectMeta,
 			taskCtx,
 		)
+
+		minReplicas := spec.Replicas
+		maxReplicas := spec.Replicas
+		if spec.MinReplicas != 0 {
+			minReplicas = spec.MinReplicas
+		}
+
+		if spec.MaxReplicas != 0 {
+			maxReplicas = spec.MaxReplicas
+		}
 
 		workerNodeRayStartParams := make(map[string]string)
 		if spec.RayStartParams != nil {
@@ -153,15 +163,6 @@ func (rayJobResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsC
 
 		if _, exists := workerNodeRayStartParams[DisableUsageStatsStartParameter]; !exists && !cfg.EnableUsageStats {
 			workerNodeRayStartParams[DisableUsageStatsStartParameter] = "true"
-		}
-
-		minReplicas := spec.MinReplicas
-		if minReplicas > spec.Replicas {
-			minReplicas = spec.Replicas
-		}
-		maxReplicas := spec.MaxReplicas
-		if maxReplicas < spec.Replicas {
-			maxReplicas = spec.Replicas
 		}
 
 		workerNodeSpec := rayv1alpha1.WorkerGroupSpec{
@@ -183,18 +184,11 @@ func (rayJobResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsC
 		rayClusterSpec.WorkerGroupSpecs[index].Template.Spec.ServiceAccountName = serviceAccountName
 	}
 
-	shutdownAfterJobFinishes := cfg.ShutdownAfterJobFinishes
-	ttlSecondsAfterFinished := &cfg.TTLSecondsAfterFinished
-	if rayJob.ShutdownAfterJobFinishes {
-		shutdownAfterJobFinishes = true
-		ttlSecondsAfterFinished = &rayJob.TtlSecondsAfterFinished
-	}
-
 	jobSpec := rayv1alpha1.RayJobSpec{
 		RayClusterSpec:           rayClusterSpec,
 		Entrypoint:               strings.Join(primaryContainer.Args, " "),
-		ShutdownAfterJobFinishes: shutdownAfterJobFinishes,
-		TTLSecondsAfterFinished:  ttlSecondsAfterFinished,
+		ShutdownAfterJobFinishes: cfg.ShutdownAfterJobFinishes,
+		TTLSecondsAfterFinished:  &cfg.TTLSecondsAfterFinished,
 		RuntimeEnv:               rayJob.RuntimeEnv,
 	}
 
@@ -511,7 +505,7 @@ func (plugin rayJobResourceHandler) GetTaskPhase(ctx context.Context, pluginCont
 	}
 
 	if len(rayJob.Status.JobDeploymentStatus) == 0 {
-		return pluginsCore.PhaseInfoQueued(time.Now(), pluginsCore.DefaultPhaseVersion, "Scheduling"), nil
+		return pluginsCore.PhaseInfoQueuedWithTaskInfo(time.Now(), pluginsCore.DefaultPhaseVersion, "Scheduling", info), nil
 	}
 
 	// KubeRay creates a Ray cluster first, and then submits a Ray job to the cluster
